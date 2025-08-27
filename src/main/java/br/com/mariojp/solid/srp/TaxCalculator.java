1b9940aa88fb573f6010b8213b201491b79bacf5
RizzoKa <kauarizzo06@gmail.com>
2025-08-27 16:57:24 -0300

    Consertando o erro no teste

--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -1,8 +1,9 @@
 package br.com.mariojp.solid.srp;
 
 public class TaxCalculator {
-	public double Total(double subtotal) {
-		double taxRate = Double.parseDouble(System.getProperty("taxRate"));
+	public double calcularTaxa(double subtotal) {
+		String taxRateString = System.getProperty("tax.rate", "0.10");
+		double taxRate = Double.parseDouble(taxRateString);
 		double tax = subtotal * taxRate; 
 		return tax;
 	}
